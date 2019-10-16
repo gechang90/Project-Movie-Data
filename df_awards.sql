dcd2a979a29a3657e52f529d227d3d8878f9b935
gechang90 <36612708+gechang90@users.noreply.github.com>
2019-10-15 20:43:39 -0400

    final version

--- df_awards.sql
+++ df_awards.sql
@@ -1,10 +1,27 @@
--- Create a new table
-CREATE TABLE movie_soup_scrap (
-  Film VARCHAR(200) NOT NULL,
-  Year VARCHAR(20) NOT NULL,
-  Awards VARCHAR(10) NOT NULL,
-  Nominations VARCHAR(10) NOT NULL
- );
- 
- -- Show the table
- SELECT* FROM movie_soup_scrap;
+
+
+
+
+-- Create Two Tables
+CREATE TABLE df_awards (
+Film VARCHAR(200) NOT NULL,
+Year VARCHAR(20) NOT NULL,
+Awards VARCHAR(10) NOT NULL,
+Nominations VARCHAR(10) NOT NULL
+);
+
+DROP TABLE IF EXISTS OMDBAPI_df;
+CREATE TABLE OMDBAPI_df (
+Title TEXT,
+Metascore VARCHAR,
+IMDBRating VARCHAR
+);
+
+-- Show the table
+SELECT* FROM df_awards;
+SELECT * FROM OMDBAPI_df;
+
+-- Joins the data
+SELECT df_awards.Film, df_awards.Year, df_awards.Awards, df_awards.Nominations, OMDBAPI_df.Metascore, OMDBAPI_df.IMDBRating
+FROM df_awards
+INNER JOIN OMDBAPI_df ON df_awards.Film=OMDBAPI_df.Title;
